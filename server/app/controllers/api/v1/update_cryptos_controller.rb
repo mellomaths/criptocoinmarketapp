1b0f011526b7e3620972664d5f66e46b08abe309
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-06-20 17:02:19 -0300

    Add post request handler for update cryptos

--- server/app/controllers/api/v1/update_cryptos_controller.rb
+++ server/app/controllers/api/v1/update_cryptos_controller.rb
@@ -3,7 +3,38 @@ module Api
         class UpdateCryptosController < ApplicationController
             
             def create 
-                render json: { cryptos: 'Ok' }, status: :ok
+                coin_amount = 10
+                currency_convertion = "BRL"
+
+                crypto_coin_market_api = CoinMarketApi.new
+                crypto_coins = crypto_coin_market_api.latest coin_amount, currency_convertion
+
+                crypto_coins.each do |coin|
+
+                    name = coin["name"]
+                    position = coin["cmc_rank"]
+                    price = coin["quote"][currency_convertion]["price"]
+                    market_cap = coin["quote"][currency_convertion]["market_cap"]
+
+                    crypto_saved = Crypto.find_by name: coin["name"]
+                    if crypto_saved
+                        crypto_saved.update_attributes({
+                            name: name,
+                            position: position,
+                            price: price,
+                            market_cap: market_cap                            
+                        })
+                    else
+                        newCrypto = Crypto.create({
+                            name: name,
+                            position: position,
+                            price: price,
+                            market_cap: market_cap
+                        })
+                    end
+                end
+
+                render json: { message: 'Cryptos loaded successfully' }, status: :ok
             end
 
         end
